832c4b727e73746488bb857704e9a19552403cc5
Fabiana Rodrigues <fabiana@getsolu.ai>
2026-05-16 18:37:06 -0300

    aula 8: funções - pt2

--- 4-funcoes/funcoes.go
+++ 4-funcoes/funcoes.go
@@ -7,6 +7,14 @@ func somar(n1 int, n2 int) int {
 	return n1 + n2
 }
 
+// Funções go podem ter mais de um retorno
+func calculosMatematicos(n1, n2 int) (int, int) {
+	soma := n1 + n2
+	subtracao := n1 - n2
+
+	return soma, subtracao
+}
+
 func main() {
 	soma := somar(56, 47)
 	fmt.Println(soma)
@@ -18,4 +26,14 @@ func main() {
 
 	fun()
 
+	fmt.Println(calculosMatematicos(10, 20))
+
+	soma, subtracao := calculosMatematicos(15, 10)
+	fmt.Println(soma, subtracao)
+
+	// podemos ignorar qualquer um dos dois retornos usando _ na declaração de variáveis
+	_, sub2 := calculosMatematicos(100, 50)
+	fmt.Println(sub2)
+	so2, _ := calculosMatematicos(150, 50)
+	fmt.Println(so2)
 }
