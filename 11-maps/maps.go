18a1e4ae1e7f22a7ee08d9976c95b9370af822e4
Fabiana Rodri <fabianarod.rodrigues@mercadolivre.com>
2026-06-10 14:25:29 -0300

    aula: maps (maps aninhados)

---
+++ 11-maps/maps.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	usuario := map[string]string {
+		"nome": "João",
+		"sobrenome": "Silva",
+		"email": "joao.silva@example.com",
+	}
+
+	aluno := map[string]map[string]string {
+		"dadosPessoais": {
+			"nome": "Maria",
+			"sobrenome": "Santos",
+			"email": "maria.santos@example.com",
+		},
+		"curso": {
+			"nome": "Engenharia de Software",
+			"instituicao": "Universidade XYZ",
+		},
+	}
+
+	fmt.Println(usuario)
+	fmt.Println(usuario["nome"])
+	fmt.Println(usuario["sobrenome"])
+	fmt.Println(usuario["email"])
+	// Para acessar os campos, não pode usar ponto, como em struct, deve usar colchetes e a chave entre aspas.
+	
+	aluno["signo"] = map[string]string {
+		"nome": "Leão",
+	}
+	fmt.Println(aluno)
+	// Para adicionar um novo campo, basta usar a sintaxe de colchetes e atribuir um valor a ele. No caso, estamos adicionando um campo "signo" ao mapa "aluno", que é um mapa aninhado com uma chave "nome" e o valor "Leão".
+}
